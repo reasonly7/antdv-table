567f71ae9332b531445ca20bd9d3a3b77c06fe22
leishanglin <reasonly8@sina.com>
2025-01-17 13:24:54 +0800

    daily updates

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,14 @@
-<script setup lang="ts">
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts" setup>
+import { ConfigProvider, DatePicker } from "ant-design-vue";
+import zhCN from "ant-design-vue/locale/zh_CN";
+import { locale } from "dayjs";
+import "dayjs/locale/zh-cn";
 
-console.log(import.meta.url)
+locale("zh-cn");
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <ConfigProvider :locale="zhCN">
+    <DatePicker></DatePicker>
+  </ConfigProvider>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
